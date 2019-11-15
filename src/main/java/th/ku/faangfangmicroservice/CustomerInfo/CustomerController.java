3f887863f75720bdb62d9175c7fb78f12d85d12b
Wachirawit <ingwachi@gmail.com>
2019-11-15 14:53:46 +0700

    Update

--- src/main/java/th/ku/faangfangmicroservice/CustomerInfo/CustomerController.java
+++ src/main/java/th/ku/faangfangmicroservice/CustomerInfo/CustomerController.java
@@ -1,9 +1,13 @@
 package th.ku.faangfangmicroservice.CustomerInfo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import th.ku.faangfangmicroservice.AvailableRoom.AvailableRoom;
+
+import java.util.List;
 
 @RestController
 public class CustomerController {
@@ -15,5 +19,10 @@ public class CustomerController {
     public String saveCustomerInfo(@RequestBody CustomerInfo customerInfo) {
         repository.save(customerInfo);
         return "Added Customer : " + customerInfo.getName();
+    }
+
+    @GetMapping("/findAllAvailableRoom")
+    public List<CustomerInfo> getDate() {
+        return repository.findAll();
     }
 }
