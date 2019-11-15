335d1f9021bc9b9269f80e904113e7a840e8254a
chanakarnf <fang-fangs@hotmail.com>
2019-11-15 19:34:03 +0700

    Update

--- src/main/java/th/ku/faangfangmicroservice/CustomerInfo/CustomerController.java
+++ src/main/java/th/ku/faangfangmicroservice/CustomerInfo/CustomerController.java
@@ -1,10 +1,7 @@
 package th.ku.faangfangmicroservice.CustomerInfo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import th.ku.faangfangmicroservice.AvailableRoom.AvailableRoom;
 
 import java.util.List;
@@ -21,8 +18,21 @@ public class CustomerController {
         return "Added Customer : " + customerInfo.getName();
     }
 
-    @GetMapping("/findAllAvailableRoom")
+    @GetMapping("/findAllCustomer")
     public List<CustomerInfo> getDate() {
         return repository.findAll();
+    }
+
+    @GetMapping("/findCustomerByPhone/{phoneNum}")
+    public CustomerInfo getByPhone(@PathVariable String phoneNum) {
+        return repository.findByPhoneNum(phoneNum);
+    }
+
+    @PutMapping("/updateStatus/{phoneNum}")
+    public CustomerInfo update(@PathVariable String phoneNum, @RequestBody CustomerInfo customerInfo) {
+        CustomerInfo record =  repository.findByPhoneNum(phoneNum);
+        record.setStatus(customerInfo.getStatus());
+        repository.save(record);
+        return record;
     }
 }
--- src/main/java/th/ku/faangfangmicroservice/CustomerInfo/CustomerInfoRepository.java
+++ src/main/java/th/ku/faangfangmicroservice/CustomerInfo/CustomerInfoRepository.java
@@ -3,4 +3,6 @@ package th.ku.faangfangmicroservice.CustomerInfo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CustomerInfoRepository extends MongoRepository<CustomerInfo, String> {
+
+    CustomerInfo findByPhoneNum(String phoneNum);
 }
